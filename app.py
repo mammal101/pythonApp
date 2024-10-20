0db0c5e6a7233625ab7c8b3ab0ef20f15c8fbc74
Alex <alexmamaev7@gmail.com>
2024-10-20 17:20:27 +0300

    second commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is my test app."
+    return "Hello, World! This is my test app. xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 if __name__ == '__main__':
     app.run(debug=True)
